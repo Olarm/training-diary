9bf3dae7be9aebf0ad45deccec1280fea08d8d4c
Ola Rasmus Moen <ola.moen@wellstarter.com>
2021-07-11 23:43:07 +0200

    Add main models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    city = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+
+    LOC_TYPES = [
+        ("G", "gym"),
+        ("A", "area"),
+    ]
+    type = models.CharField(max_length=1, choices=LOC_TYPES, default="A")
